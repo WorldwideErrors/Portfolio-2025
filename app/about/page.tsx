0d1679fbb32d885a23fb54aed3cc1a39e48b89af
Jeffrey <jeffreyvantillo@gmail.com>
2025-12-01 14:35:35 +0100

    Rewrite the metadata to the page and load the client in a different component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,104 +1,10 @@
-"use client";
+import { Metadata } from 'next';
+import AboutClient from './AboutClient';
 
-import { AnimatePresence, motion, Variants } from 'framer-motion'
-import React from 'react'
-import Container from '../components/Container';
-import Title from '../components/Title';
-import HomeButton from '../components/HomeButton';
-import Link from 'next/link';
-import { angkor, lux_roman } from '../fonts';
-import Image from 'next/image';
-
-export const metadata = {
-  title: 'About Me',
-};
-
-const navContainerVariants: Variants = {
-  hidden: {},
-  show: {
-    transition: { staggerChildren: 0.3 },
-  },
-};
-
-const Variants_L: Variants = {
-  hidden: {opacity: 0, x:-50},
-  show: {
-    x:0,
-    opacity: 1,
-    transition: { duration: 0.45, staggerChildren: 0.3, ease: "easeOut"  },
-  }
-};
-
-const Variants_R: Variants = {
-  hidden: {opacity: 0, x:50},
-  show: {
-    x:0,
-    opacity: 1,
-    transition: { duration: 0.45, staggerChildren: 0.3, ease: "easeOut"  },
-  }
+export const metadata: Metadata = {
+  title: 'About Me'
 };
 
 export default function Page() {
-  return (
-      <div className="p-6 lg:p-10 ">
-        <Container>
-          <motion.div
-            variants={navContainerVariants}
-            initial="hidden"
-            animate="show"
-          >
-            <Title name="about" />
-            <Link href="/">
-            <HomeButton />
-            </Link>
-          </motion.div>
-        </Container>
-        <Container>
-            <div className='grid grid-cols-12 gap-6 pt-10'>
-              <AnimatePresence mode="wait">
-                <motion.div className='col-span-12 md:col-span-6'
-                variants={Variants_L}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: true }} 
-                >
-                  <Image
-                  src="/propedeuse.JPG"
-                  alt="Jeffrey van Tillo"
-                  width={400}
-                  height={300}
-                  className="rounded-xl shadow-xl"
-                  />
-                </motion.div>
-              </AnimatePresence>
-              <motion.div className='col-span-12 md:col-span-6'
-              variants={Variants_R}
-              initial="hidden"
-              whileInView="show"
-              viewport={{ once: true }} >
-                <h1 className={`text-1xl sm:text-2xl md:text-2xl lg:text-5xl ${angkor.className}`}>
-                  Hi! My name is...
-                </h1>
-                <p className={`text-2xl ${lux_roman.className}`}>
-                  ...Jeffrey! A passionate and dedicated react developer. I love creating beautiful and functional web applications 
-                  that provide a trustworthy user experience. With a strong foundation in both front-end and back-end development, 
-                  I offer my perspective on the entire development process. I am constantly trying to improve my skills and stay up-to-date
-                  with the latest trends of the industry. 
-                </p>
-              </motion.div>
-              <motion.div className='col-span-12 md:col-span-8 pt-10 flex items-end gap-6'
-              variants={Variants_L}
-              initial="hidden"
-              whileInView="show">
-                <p className={`text-2xl ${lux_roman.className}`}>
-                  When I&apos;m not coding, you probably have to find me somewhere with Google Maps... I can be at quite some places.
-                  In my free time I fullfill my passion for moving by going to the gym. I also enjoy spending time with friends and family,
-                  exploring new places, and indulging in my love for gaming. None of those activities can be done without a good soundtrack, 
-                  thanks to technology, we can take our music everywhere these days. 
-                </p>
-              </motion.div>
-            </div>
-        </Container>
-      </div>
-    );
+  return <AboutClient />;
 }
---
+++ app/case-studies/CaseStudiesClient.tsx
@@ -0,0 +1,187 @@
+"use client";
+
+import { AnimatePresence, motion, Variants } from 'framer-motion'
+import React, { useState } from 'react'
+import Container from '../components/Container';
+import Title from '../components/Title';
+import HomeButton from '../components/HomeButton';
+import CaseStudieItem from '../components/CaseStudieItem';
+import Image from 'next/image';
+
+import { CaseStudies } from '../../lib/case-studies';
+import Link from 'next/link';
+import { angkor, lux_roman } from '../fonts';
+import { FaNode, FaPaperPlane, FaReact } from 'react-icons/fa';
+import { SiNextdotjs } from 'react-icons/si';
+import { BiLogoTypescript } from 'react-icons/bi';
+import { RiAngularjsFill, RiJavaLine } from 'react-icons/ri';
+
+export const metadata = {
+  title: 'Case Studies',
+};
+
+const navContainerVariants: Variants = {
+  hidden: { opacity: 0.0 },
+  show: {
+    opacity: 1,
+    transition: { duration: 0.45, ease: "easeOut", repeat: 0 },
+  },
+};
+
+const casestudiesVariants: Variants = {
+  hidden: {opacity: 0, x:50},
+  show: {
+    x:0,
+    opacity: 1,
+    transition: { duration: 0.45, staggerChildren: 0.3 },
+  },
+  whileTap: { scale: 0.95 },
+};
+
+const casestudiesVariants_L: Variants = {
+  hidden: {opacity: 0, x:-50},
+  show: {
+    x:0,
+    opacity: 1,
+    transition: { duration: 0.45, staggerChildren: 0.3, ease: "easeOut"  },
+  }
+};
+
+const casestudiesVariants_R: Variants = {
+  hidden: {opacity: 0, x:50},
+  show: {
+    x:0,
+    opacity: 1,
+    transition: { duration: 0.45, staggerChildren: 0.3, ease: "easeOut"  },
+  }
+};
+
+const techIcons: { [key: string]: React.ReactNode } = {
+  React: <div className='shadow-xl bg-[var(--color-foreground)] rounded-xl'><FaReact className="m-2 text-[var(--color-secondary)]"/></div>,
+  TypeScript: <div className='shadow-xl bg-[var(--color-foreground)] rounded-xl'><BiLogoTypescript className="m-2 text-[var(--color-secondary)]" /></div>,
+  "Next.js": (
+  <div className='shadow-xl bg-[var(--color-foreground)] rounded-xl'>
+    <SiNextdotjs className="m-2 text-[var(--color-secondary)]"/>
+  </div>
+),
+  Java: <div className='shadow-xl bg-[var(--color-foreground)] rounded-xl'><RiJavaLine className="m-2 text-[var(--color-secondary)]" /></div>,
+  Paper: <div className='shadow-xl bg-[var(--color-foreground)] rounded-xl'><FaPaperPlane className="m-2 text-[var(--color-secondary)]" /></div>,
+  Angular: <div className='shadow-xl bg-[var(--color-foreground)] rounded-xl'><RiAngularjsFill className="m-2 text-[var(--color-secondary)]"/></div>,
+  NodeJS: <div className='shadow-xl bg-[var(--color-foreground)] rounded-xl'><FaNode className="m-2 text-[var(--color-secondary)]"/></div>,
+};
+
+
+
+export default function Page() {
+  const [selectedCase, setSelectedCase] = useState(CaseStudies[0]);
+  
+  return (
+      <div className="p-6 lg:p-10">
+        <Container>
+          <motion.div
+            variants={navContainerVariants}
+            initial="hidden"
+            animate="show"
+            className='left-0 top-0'
+          >
+            <Title name="casestudies"  />
+          </motion.div>
+          <Link href="/">
+            <HomeButton />
+          </Link>
+        </Container>
+        <Container>
+          <div className='grid grid-cols-12 gap-6 pt-10'>
+            <motion.div className='col-span-7'
+              key={selectedCase.id}
+              variants={casestudiesVariants_L}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: true }} >
+              <p className={`text-2xl ${lux_roman.className}`}>
+              {selectedCase.content.heading}
+              </p>
+              <div>
+                <div
+                  className="flex flex-wrap self-end gap-4 mt-2"
+                >
+                  {selectedCase.techstack.map((tech) => (
+                    <span key={tech} className="lg:text-4xl text-2xl flex pr-2 pt-2  ">
+                      {techIcons[tech] || tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+            </motion.div>
+            <div className="col-span-5 flex flex-col overflow-hidden">
+              {CaseStudies.map((item) => (
+                <motion.button
+                variants={casestudiesVariants}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: true }} 
+                whileTap={selectedCase.id !== item.id ? { scale: 0.95, opacity: 0.8 } : {}}
+                  key={item.id}
+                  onClick={() => setSelectedCase(item)}
+                  className={`text-left text-nowrap transition-colors  ${
+                    selectedCase.id === item.id
+                      ? "text-secondary font-semibold "
+                      : "text-primary hover:text-secondary cursor-pointer"
+                  }`}
+                >
+                  <CaseStudieItem>{item.title}</CaseStudieItem>
+                </motion.button>
+              ))}
+            </div>
+            
+            <div className='row-span-1 col-span-12 grid grid-cols-12 gap-6 pt-32'>
+              <AnimatePresence mode="wait">
+              <motion.div className='col-span-6'
+              key={selectedCase.id}
+              variants={casestudiesVariants_L}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: true }} 
+              >
+                <Image
+                src={selectedCase.content.image}
+                alt={selectedCase.title}
+                width={600}
+                height={300}
+                className="rounded-xl shadow-xl"
+                />
+              </motion.div>
+              </AnimatePresence>
+              <motion.div className='col-span-6'
+              key={selectedCase.id}
+              variants={casestudiesVariants_R}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: true }} 
+              >
+                <p className={`text-2xl ${lux_roman.className}`}>
+                  {selectedCase.content.body}
+                </p>
+              </motion.div>
+            </div>
+            <div className='row-span-1 col-span-12 grid grid-cols-12 gap-6 pt-32'>
+              <div className='col-span-12'>
+                <p className={`text-2xl mb-4 ${lux_roman.className}`}>
+                  {selectedCase.content.conclusion}
+                </p>
+              </div>
+            </div>
+            <div className='row-span-1 col-span-12 grid grid-cols-12 gap-6 '>
+              <div className='col-span-12 flex justify-center'>
+                <Link target="_blank" href={selectedCase.url} className='bg-[var(--color-foreground)] p-3 rounded-xl shadow-xl'>
+                    <span className={`${angkor.className} text-2xl text-[var(--color-secondary)] hover:text-[var(--color-accent)] transition-colors duration-300`}>OPEN IN GITHUB</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+    
+}
